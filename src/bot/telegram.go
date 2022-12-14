fd32a8d8f0f4e8b0d26c89241a153008ab49bb00
walker088 <walker088@walker088.tw>
2022-12-14 11:31:57 -0300

    feat: Make Bot struct to use only private fields

--- src/bot/telegram.go
+++ src/bot/telegram.go
@@ -11,9 +11,9 @@ import (
 )
 
 type Bot struct {
-	AllowedUsers []int64
-	Api          *tgbotapi.BotAPI
-	Timeout      int
+	allowedUsers []int64
+	api          *tgbotapi.BotAPI
+	timeout      int
 	engine       chatgpt.ChatGPT
 }
 
@@ -26,17 +26,17 @@ func NewBot(engine chatgpt.ChatGPT, cfg *config.AppConfig) (*Bot, error) {
 	botapi.Debug = cfg.BotDebug
 
 	return &Bot{
-		AllowedUsers: cfg.AllowdUsers,
-		Api:          botapi,
-		Timeout:      cfg.BotTimeout,
+		allowedUsers: cfg.AllowdUsers,
+		api:          botapi,
+		timeout:      cfg.BotTimeout,
 		engine:       engine,
 	}, nil
 }
 
 func (b *Bot) GetUpdatesChan() tgbotapi.UpdatesChannel {
 	u := tgbotapi.NewUpdate(0)
-	u.Timeout = b.Timeout
-	return b.Api.GetUpdatesChan(u)
+	u.Timeout = b.timeout
+	return b.api.GetUpdatesChan(u)
 }
 
 func (b *Bot) HandleUpdate(update tgbotapi.Update) {
@@ -46,19 +46,22 @@ func (b *Bot) HandleUpdate(update tgbotapi.Update) {
 		updateMessageID = update.Message.MessageID
 		updateUserID    = update.Message.From.ID
 	)
-	if len(b.AllowedUsers) != 0 && !utils.Contains(b.AllowedUsers, updateUserID) {
+	if _, err := b.api.Request(tgbotapi.NewChatAction(updateChatID, "typing")); err != nil {
+		log.Printf("Couldn't send typing action: %v", err)
+	}
+	if len(b.allowedUsers) != 0 && !utils.Contains(b.allowedUsers, updateUserID) {
 		log.Printf("User %d is not allowed to use the bot", updateUserID)
 		var reply = "You are not allowed to use the bot, please contact the provider for further info ヾ(=ﾟ･ﾟ=)ﾉ"
 		replyMsg := tgbotapi.NewMessage(updateChatID, reply)
-		_, _ = b.Api.Send(replyMsg)
+		_, _ = b.api.Send(replyMsg)
 	} else {
 		var reply, _ = b.engine.AskQuestion(updateText)
 		replyMsg := tgbotapi.NewMessage(updateChatID, string(reply))
 		replyMsg.ReplyToMessageID = updateMessageID
-		_, _ = b.Api.Send(replyMsg)
+		_, _ = b.api.Send(replyMsg)
 	}
 }
 
 func (b *Bot) Stop() {
-	b.Api.StopReceivingUpdates()
+	b.api.StopReceivingUpdates()
 }
